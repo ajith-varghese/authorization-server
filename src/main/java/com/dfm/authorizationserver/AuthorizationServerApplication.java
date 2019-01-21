09960b149a7d6132b5f176691e38ea0f0035d21c
Ajith Varghese <ajith.jv.dev@gmail.com>
2019-01-21 14:14:10 -0500

    first implementation

--- src/main/java/com/dfm/authorizationserver/AuthorizationServerApplication.java
+++ src/main/java/com/dfm/authorizationserver/AuthorizationServerApplication.java
@@ -2,8 +2,13 @@ package com.dfm.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dfm/authorizationserver/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.dfm.authorizationserver.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@RequestMapping("/user")
+	Principal getUser(Principal user) {
+		return user;
+	}
+}
